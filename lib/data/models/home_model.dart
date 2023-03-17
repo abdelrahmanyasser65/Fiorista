381767d3ebcaf5db7914fbfe9ede5894ee66e839
abdelrahmanyasser65 <abdelrahman117ma@gmail.com>
2023-03-17 17:26:28 +0200

    category

---
+++ lib/data/models/home_model.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:fiorista/presentation/resources/strings.dart';
+
+List category=[
+  Strings.all,
+  Strings.plant,
+  Strings.tree,
+  Strings.palm,
+  Strings.seeds,
+  Strings.tools
+];
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fiorista/data/models/home_model.dart';
 import 'package:fiorista/presentation/resources/colors.dart';
 import 'package:fiorista/presentation/resources/fonts.dart';
 import 'package:fiorista/presentation/resources/images.dart';
@@ -5,9 +6,15 @@ import 'package:fiorista/presentation/resources/strings.dart';
 import 'package:fiorista/presentation/resources/values.dart';
 import 'package:fiorista/presentation/widget/default_text.dart';
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex=0;
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -54,9 +61,48 @@ class HomeScreen extends StatelessWidget {
                     text: Strings.search,
                     color: ColorManager.grey,
                     fontSize: FontSized.fs14,
-                    fontWeight: FontWeightManager.medium)
+                    fontWeight: FontWeightManager.medium),
+
               ],
             ),
+          ),
+          SizedBox(
+            height: HeightSized.s5,
+
+            child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                physics:const BouncingScrollPhysics(),
+                itemBuilder: (context,index){
+                  return InkWell(
+                    onTap: (){
+                      setState(() {
+                        currentIndex=index;
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: HeightSized.s2,
+                        vertical: HeightSized.s1
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color:currentIndex==index? ColorManager.primary:
+                              ColorManager.black.withOpacity(0.05)
+                      ),
+                      child: DefaultText(
+                        text: category[index],
+                        color: currentIndex==index?ColorManager.white:ColorManager.primary,
+                        fontWeight: FontWeightManager.semiBold,
+                        fontSize: FontSized.fs16,
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context,index)=>SizedBox(
+                  width: HeightSized.s2,
+                ),
+                itemCount: category.length),
           ),
         ],
       ),
